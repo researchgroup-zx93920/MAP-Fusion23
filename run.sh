e53fb3488e0710bb7074719a5e249368f4a6f10b
Samiran Kawtikwar <samkawtikwar@gmail.com>
2023-02-14 13:33:27 -0600

    Baseline

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
-module load mpi
+# module load mpi
+# make clean all
+# COST_PATH=/home/samiran2/Downloads/new/Multi-dimensional-Assignment-master/
 
-COST_PATH=/home/samiran2/Downloads/new/Multi-dimensional-Assignment-master/
-
-mpirun -np 1 test 5 5 1 100 0 0 0 1 ${COST_PATH}/Costs/costs_linear_triplet_K5_N5-0.txt ${COST_PATH}/Costs/outputcosts_Samhita_K5_N5_S0_problem0.txt
+# mpirun -np 1 test 5 5 1 100 0 0 0 1 ${COST_PATH}/Costs/costs_linear_triplet_K5_N5-0.txt ${COST_PATH}/Costs/outputcosts_Samhita_K5_N5_S0_problem0.txt
+mpirun -np 1 ./test $1 $1 1 100 0 0 0 0 0 0
